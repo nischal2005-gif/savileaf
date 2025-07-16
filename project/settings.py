b0316f70085b8370219fd084eaa1567bee47137c
savileaf <nischal@savileaf.com>
2025-07-16 13:19:54 +0545

    added whitenoise

--- project/settings.py
+++ project/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
